949c17fd876f08f8d021951ffb7fa598b2294fef
Mark Vrooland <mark@vrooland.net>
2021-12-02 14:23:19 +0100

    Database fix

--- ps-database/insert-domain-initial-setup-tables.sql
+++ ps-database/insert-domain-initial-setup-tables.sql
@@ -24,14 +24,14 @@ INSERT INTO public.page (id, domain_id, page_type_id, name, layout_type) VALUES 
 INSERT INTO public.page (id, domain_id, page_type_id, name, layout_type) VALUES (6, 1, 6, 'Nieuwe melding - controle', 'page');
 
 
-INSERT INTO public.page_button (id, page_id, location, button_type_id, name, route, action_type_id, sort) VALUES (1, 2, 'left', 2, 'Vorige', '', 12, 0);
-INSERT INTO public.page_button (id, page_id, location, button_type_id, name, route, action_type_id, sort) VALUES (2, 4, 'left', 2, 'Annuleren', '', 13, 0);
-INSERT INTO public.page_button (id, page_id, location, button_type_id, name, route, action_type_id, sort) VALUES (3, 4, 'right', 1, 'Volgende', '/new/information', 14, 0);
-INSERT INTO public.page_button (id, page_id, location, button_type_id, name, route, action_type_id, sort) VALUES (4, 5, 'right', 1, 'Controleer de melding', '/new/confirmation', 14, 0);
-INSERT INTO public.page_button (id, page_id, location, button_type_id, name, route, action_type_id, sort) VALUES (5, 5, 'left', 2, 'Vorige', '', 12, 0);
-INSERT INTO public.page_button (id, page_id, location, button_type_id, name, route, action_type_id, sort) VALUES (6, 6, 'left', 2, 'Annuleren', '', 13, 0);
-INSERT INTO public.page_button (id, page_id, location, button_type_id, name, route, action_type_id, sort) VALUES (7, 6, 'left', 2, 'Vorige', '', 12, 1);
-INSERT INTO public.page_button (id, page_id, location, button_type_id, name, route, action_type_id, sort) VALUES (8, 6, 'right', 1, 'Melding maken', '', 2, 0);
+INSERT INTO public.page_button (id, page_id, location, button_type_id, name, route, action_type_id, sort) VALUES (nextval('public.seq_page_button_id'), 2, 'left', 2, 'Vorige', '', 12, 0);
+INSERT INTO public.page_button (id, page_id, location, button_type_id, name, route, action_type_id, sort) VALUES (nextval('public.seq_page_button_id'), 4, 'left', 2, 'Annuleren', '', 13, 0);
+INSERT INTO public.page_button (id, page_id, location, button_type_id, name, route, action_type_id, sort) VALUES (nextval('public.seq_page_button_id'), 4, 'right', 1, 'Volgende', '/new/information', 14, 0);
+INSERT INTO public.page_button (id, page_id, location, button_type_id, name, route, action_type_id, sort) VALUES (nextval('public.seq_page_button_id'), 5, 'right', 1, 'Controleer de melding', '/new/confirmation', 14, 0);
+INSERT INTO public.page_button (id, page_id, location, button_type_id, name, route, action_type_id, sort) VALUES (nextval('public.seq_page_button_id'), 5, 'left', 2, 'Vorige', '', 12, 0);
+INSERT INTO public.page_button (id, page_id, location, button_type_id, name, route, action_type_id, sort) VALUES (nextval('public.seq_page_button_id'), 6, 'left', 2, 'Annuleren', '', 13, 0);
+INSERT INTO public.page_button (id, page_id, location, button_type_id, name, route, action_type_id, sort) VALUES (nextval('public.seq_page_button_id'), 6, 'left', 2, 'Vorige', '', 12, 1);
+INSERT INTO public.page_button (id, page_id, location, button_type_id, name, route, action_type_id, sort) VALUES (nextval('public.seq_page_button_id'), 6, 'right', 1, 'Melding maken', '', 2, 0);
 
 --
 -- TOC entry 3159 (class 0 OID 37433)
@@ -39,49 +39,43 @@ INSERT INTO public.page_button (id, page_id, location, button_type_id, name, rou
 -- Data for Name: page_overview; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.page_overview (id, page_id, name, icon, toggle, route, size, priority, personal, sort) VALUES (1, 1, 'Nieuwe meldingen', 'fiber_new', true, 'details/${calllist.id}', 50, true, false, 0);
-INSERT INTO public.page_overview (id, page_id, name, icon, toggle, route, size, priority, personal, sort) VALUES (2, 1, 'In behandeling', 'event', false, 'details/${calllist.id}', 50, true, true, 1);
-INSERT INTO public.page_overview (id, page_id, name, icon, toggle, route, size, priority, personal, sort) VALUES (3, 1, 'Zoek een melding', 'search', false, 'details/${calllist.id}', 50, false, false, 2);
+INSERT INTO public.page_overview (id, page_id, name, icon, toggle, route, size, priority, personal, sort) VALUES (nextval('public.seq_page_overview_id'), 1, 'Nieuwe meldingen', 'fiber_new', true, 'details/${calllist.id}', 50, true, false, 0);
 
---
--- TOC entry 3160 (class 0 OID 37463)
--- Dependencies: 244
--- Data for Name: page_overview_status; Type: TABLE DATA; Schema: public; Owner: postgres
---
+INSERT INTO public.page_overview_status (id, page_overview_id, status_id) VALUES (1, currval('public.seq_page_overview_id'), 1);
 
-INSERT INTO public.page_overview_status (id, page_overview_id, status_id) VALUES (1, 1, 1);
-INSERT INTO public.page_overview_status (id, page_overview_id, status_id) VALUES (2, 2, 2);
-INSERT INTO public.page_overview_status (id, page_overview_id, status_id) VALUES (3, 3, 1);
-INSERT INTO public.page_overview_status (id, page_overview_id, status_id) VALUES (4, 3, 2);
-INSERT INTO public.page_overview_status (id, page_overview_id, status_id) VALUES (5, 3, 3);
+INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), currval('public.seq_page_overview_id'), 'category', 'Categorie', 'string', 'equals', 'col-sm-12 col-md-2 col-lg-2 bold', 'one', 0);
+INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), currval('public.seq_page_overview_id'), 'location', 'Lokatie', 'string', '', 'col-sm-6 col-md-2 col-lg-2 italic', 'two', 1);
+INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), currval('public.seq_page_overview_id'), 'casenumber', 'Id', 'string', '', 'col-sm-6 col-md-1 col-lg-1', 'three-a', 2);
+INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), currval('public.seq_page_overview_id'), 'description', 'Omschrijving', 'text', '', 'col-sm-12 col-md-4 col-lg-4', '', 3);
+INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), currval('public.seq_page_overview_id'), 'dateCreated', 'Datum', 'date', '', 'col-sm-6 col-md-2 col-lg-2', 'three-b', 4);
+INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), currval('public.seq_page_overview_id'), 'area', 'Gebied', 'string', '', 'col-md-1 col-lg-1', '', 5);
 
 
---
--- TOC entry 3159 (class 0 OID 37448)
--- Dependencies: 242
--- Data for Name: page_overview_column; Type: TABLE DATA; Schema: public; Owner: postgres
---
+INSERT INTO public.page_overview (id, page_id, name, icon, toggle, route, size, priority, personal, sort) VALUES (nextval('public.seq_page_overview_id'), 1, 'In behandeling', 'event', false, 'details/${calllist.id}', 50, true, true, 1);
 
-INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), 1, 'category', 'Categorie', 'string', 'equals', 'col-sm-12 col-md-2 col-lg-2 bold', 'one', 0);
-INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), 1, 'location', 'Lokatie', 'string', '', 'col-sm-6 col-md-2 col-lg-2 italic', 'two', 1);
-INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), 1, 'casenumber', 'Id', 'string', '', 'col-sm-6 col-md-1 col-lg-1', 'three-a', 2);
-INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), 1, 'description', 'Omschrijving', 'text', '', 'col-sm-12 col-md-4 col-lg-4', '', 3);
-INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), 1, 'dateCreated', 'Datum', 'date', '', 'col-sm-6 col-md-2 col-lg-2', 'three-b', 4);
-INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), 1, 'area', 'Gebied', 'string', '', 'col-md-1 col-lg-1', '', 5);
+INSERT INTO public.page_overview_status (id, page_overview_id, status_id) VALUES (2, currval('public.seq_page_overview_id'), 2);
 
-INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), 2, 'category', 'Categorie', 'string', 'equals', 'col-sm-12 col-md-2 col-lg-2 bold', 'one', 0);
-INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), 2, 'location', 'Lokatie', 'string', '', 'col-sm-6 col-md-2 col-lg-2 italic', 'two', 1);
-INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), 2, 'casenumber', 'Id', 'string', '', 'col-sm-6 col-md-1 col-lg-1', 'three-a', 2);
-INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), 2, 'description', 'Omschrijving', 'text', '', 'col-sm-12 col-md-4 col-lg-4', '', 3);
-INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), 2, 'dateCreated', 'Datum', 'date', '', 'col-sm-6 col-md-2 col-lg-2', 'three-b', 4);
-INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), 2, 'area', 'Gebied', 'string', '', 'col-md-1 col-lg-1', '', 5);
+INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), currval('public.seq_page_overview_id'), 'category', 'Categorie', 'string', 'equals', 'col-sm-12 col-md-2 col-lg-2 bold', 'one', 0);
+INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), currval('public.seq_page_overview_id'), 'location', 'Lokatie', 'string', '', 'col-sm-6 col-md-2 col-lg-2 italic', 'two', 1);
+INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), currval('public.seq_page_overview_id'), 'casenumber', 'Id', 'string', '', 'col-sm-6 col-md-1 col-lg-1', 'three-a', 2);
+INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), currval('public.seq_page_overview_id'), 'description', 'Omschrijving', 'text', '', 'col-sm-12 col-md-4 col-lg-4', '', 3);
+INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), currval('public.seq_page_overview_id'), 'dateCreated', 'Datum', 'date', '', 'col-sm-6 col-md-2 col-lg-2', 'three-b', 4);
+INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), currval('public.seq_page_overview_id'), 'area', 'Gebied', 'string', '', 'col-md-1 col-lg-1', '', 5);
 
-INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), 3, 'category', 'Categorie', 'string', 'equals', 'col-sm-12 col-md-2 col-lg-2 bold', 'one', 0);
-INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), 3, 'location', 'Lokatie', 'string', '', 'col-sm-6 col-md-2 col-lg-2 italic', 'two', 1);
-INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), 3, 'casenumber', 'Id', 'string', '', 'col-sm-6 col-md-1 col-lg-1', 'three-a', 2);
-INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), 3, 'description', 'Omschrijving', 'text', '', 'col-sm-12 col-md-4 col-lg-4', '', 3);
-INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), 3, 'dateCreated', 'Datum', 'date', '', 'col-sm-6 col-md-2 col-lg-2', 'three-b', 4);
-INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), 3, 'area', 'Gebied', 'string', '', 'col-md-1 col-lg-1', '', 5);
+
+INSERT INTO public.page_overview (id, page_id, name, icon, toggle, route, size, priority, personal, sort) VALUES (nextval('public.seq_page_overview_id'), 1, 'Zoek een melding', 'search', false, 'details/${calllist.id}', 50, false, false, 2);
+
+INSERT INTO public.page_overview_status (id, page_overview_id, status_id) VALUES (3, currval('public.seq_page_overview_id'), 1);
+INSERT INTO public.page_overview_status (id, page_overview_id, status_id) VALUES (4, currval('public.seq_page_overview_id'), 2);
+INSERT INTO public.page_overview_status (id, page_overview_id, status_id) VALUES (5, currval('public.seq_page_overview_id'), 3);
+
+
+INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), currval('public.seq_page_overview_id'), 'category', 'Categorie', 'string', 'equals', 'col-sm-12 col-md-2 col-lg-2 bold', 'one', 0);
+INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), currval('public.seq_page_overview_id'), 'location', 'Lokatie', 'string', '', 'col-sm-6 col-md-2 col-lg-2 italic', 'two', 1);
+INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), currval('public.seq_page_overview_id'), 'casenumber', 'Id', 'string', '', 'col-sm-6 col-md-1 col-lg-1', 'three-a', 2);
+INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), currval('public.seq_page_overview_id'), 'description', 'Omschrijving', 'text', '', 'col-sm-12 col-md-4 col-lg-4', '', 3);
+INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), currval('public.seq_page_overview_id'), 'dateCreated', 'Datum', 'date', '', 'col-sm-6 col-md-2 col-lg-2', 'three-b', 4);
+INSERT INTO public.page_overview_column (id, page_overview_id, name, title, type, filter, css, mobile, sort) VALUES (nextval('public.seq_page_overview_column_id'), currval('public.seq_page_overview_id'), 'area', 'Gebied', 'string', '', 'col-md-1 col-lg-1', '', 5);
 
 
 --
--- ps-database/insert-test-data.sql
+++ ps-database/insert-test-data.sql
@@ -23,6 +23,6 @@ INSERT INTO public.category (id, main_category_id, name, start_date, end_date, a
 INSERT INTO public.category (id, main_category_id, name, start_date, end_date, active, group_id) VALUES (nextval('public.seq_category_id'), 3, 'Hangjongeren', '2021-01-01', NULL, true, 1);
 
 
-INSERT INTO public.call (id, description, category_id, casenumber, company_id, status_id) VALUES (nextval('public.seq_call_id'), 'Testmelding 1', 1, '1000000', 1, 1);
+INSERT INTO public.call (id, description, category_id, casenumber, company_id, status_id, group_id) VALUES (nextval('public.seq_call_id'), 'Testmelding 1', 1, '1000000', 1, 1, 1);
 INSERT INTO public.location (id, street, number, letter, postal, city, area, x, y, call_id) VALUES ( nextval( 'public.seq_location_id'), 'Piratenweg', '1' ,'a', '2121AA', 'Hoofddorp', 1, 0, 0, currval('public.seq_call_id'));
 
